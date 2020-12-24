23af26c72b4be77582e658f9e8ea08f26229ad26
Sunanda <sunandasomwase@gmail.com>
2020-12-24 10:17:10 +0530

    maximum subarray

---
+++ maxSubarray.cpp
@@ -0,0 +1,11 @@
+int maxSubArray(vector<int>& nums) {
+        int maxSub = nums[0], curSum = 0;
+        
+        for (int n : nums) {
+            if (curSum < 0)
+                curSum = 0;
+            curSum += n;
+            maxSub = max(maxSub, curSum);
+        }
+        return maxSub;
+    }
