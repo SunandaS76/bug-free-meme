e39d65fc17388237456d067f8ff1ed5d412a8258
Sunanda <sunandasomwase@gmail.com>
2021-07-30 23:29:56 +0530

    Intersection of Two Arrays

--- Intersection_of_Two_Arrays.cpp
+++ Intersection_of_Two_Arrays.cpp
@@ -1,3 +1,4 @@
+// Using unordered map
 vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int, int>freq1;
         vector<int>::iterator found;
@@ -11,6 +12,28 @@ vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
             if(found != nums2.end())
             {
                  results.push_back(it->first);
+                
+            }
+        }
+        return results;
+    }
+
+
+    //  Using ordered set
+
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+       unordered_set<int> freq1;
+        vector<int>::iterator found;
+        vector<int> results;
+        for(int i=0; i<nums1.size(); i++)
+            freq1.insert(nums1[i]);
+        
+        for(auto it = freq1.begin(); it != freq1.end(); ++it)
+        {
+          found = find(nums2.begin(),nums2.end(),*it);
+            if(found != nums2.end())
+            {
+                 results.push_back(*it);
                 
             }
         }
