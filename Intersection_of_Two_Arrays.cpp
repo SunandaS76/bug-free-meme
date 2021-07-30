d5419422747727280e3a39e64b3b1714775646f7
Sunanda <sunandasomwase@gmail.com>
2021-07-30 23:26:27 +0530

    Intersection of Two Arrays

---
+++ Intersection_of_Two_Arrays.cpp
@@ -0,0 +1,18 @@
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int>freq1;
+        vector<int>::iterator found;
+        vector<int> results;
+        for(int i=0; i<nums1.size(); i++)
+            freq1[nums1[i]]++;
+        
+        for(auto it = freq1.begin(); it != freq1.end(); ++it)
+        {
+          found = find(nums2.begin(),nums2.end(),it->first);
+            if(found != nums2.end())
+            {
+                 results.push_back(it->first);
+                
+            }
+        }
+        return results;
+    }
