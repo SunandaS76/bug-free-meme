88de1e60727f15972ba892d7d32cf9c111cc8aff
Sunanda <sunandasomwase@gmail.com>
2021-08-12 15:48:22 +0530

    path sum

--- Path_Sum.cpp
+++ Path_Sum.cpp
@@ -1,3 +1,4 @@
+// Using bottom to down boolean approch
 bool hasPathSum(TreeNode* root, int targetSum) {
         if(!root) return false;
         if(root->left == NULL && root->right == NULL)
@@ -6,3 +7,22 @@ bool hasPathSum(TreeNode* root, int targetSum) {
         }
         return hasPathSum(root->right, targetSum - root->val) || hasPathSum(root->left, targetSum - root->val);
     }
+
+// Using botton to down void approch
+
+class Solution {
+    
+public:
+    bool res = false;
+    bool hasPathSum(TreeNode* root, int targetSum) {
+       dfs(root, targetSum);
+        return res;
+    }
+    void dfs(TreeNode* root, int sum){
+        if(!root) return ;
+        if(root->left == NULL && root->right == NULL && root->val == sum) res = true;
+        
+        dfs(root->left, sum - root->val);
+        dfs(root->right, sum - root->val);
+    }
+};
