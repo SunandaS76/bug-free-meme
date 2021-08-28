885d2acb603a0e01aeb746325b6f7f4ab0094b9e
Sunanda <sunandasomwase@gmail.com>
2021-08-28 16:13:29 +0530

    maximum subarray

--- maxSubarray.cpp
+++ maxSubarray.cpp
@@ -1,3 +1,4 @@
+// Method one using O(1) Space
 int maxSubArray(vector<int>& nums) {
         int maxSub = nums[0], curSum = 0;
         
@@ -8,4 +9,18 @@ int maxSubArray(vector<int>& nums) {
             maxSub = max(maxSub, curSum);
         }
         return maxSub;
+    }
+
+// Method two using O(N) extra space
+
+int maxSubArray(vector<int>& nums) {
+        int i, n = nums.size();
+        vector<long long> dp(n+1, 0);
+        long long ans = INT_MIN;
+        for(int i=0; i<n; i++){
+            dp[i + 1] = nums[i];
+            dp[i + 1] = max(dp[i + 1], nums[i] + dp[i]);
+            ans = max(ans, dp[i + 1]);
+        }
+        return ans;
     }
