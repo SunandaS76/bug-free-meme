0757dd7dad3424d905ee4c517ff8ff3089513a50
Sunanda <sunandasomwase@gmail.com>
2021-09-01 20:59:32 +0530

    fibbonaci series

---
+++ fibbonaci_number.cpp
@@ -0,0 +1,14 @@
+ int fib(int n) {
+       vector<int>dp(n+1);
+        
+        dp[0] = 0;
+        if(n==0)
+            return dp[n];
+        dp[1] = 1;
+        
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+       
+    }
