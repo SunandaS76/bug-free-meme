43ce0d3fb6991ec59afb412a098760c109d1e4f6
sunanda-vs <22m2107@iitb.ac.in>
2023-11-02 11:29:22 +0530

    getline example

---
+++ input_output.cpp
@@ -0,0 +1,15 @@
+// Get line example
+// Take multiple words in a line together
+
+#include<iostream>
+#include<string.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string str ; 
+    getline(cin, str);
+    cout << "Here is the string : "<<str <<endl;
+    return 0;
+}
